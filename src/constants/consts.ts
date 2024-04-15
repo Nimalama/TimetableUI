4dafa3b0476716859b7530874bf8f01b6c49c8c9
beingsuman7 <sumanempire7@gmail.com>
2024-04-15 11:13:43 +1000

    Add course

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -2,3 +2,37 @@ export const MODAL_TYPES = {
   CREATE_MODE: 'create',
   EDIT_MODE: 'edit'
 };
+ 
+export const courses = [
+  {
+    imageUrl: 'https://3rdwavemedia.com/demo-images/slides/maker-module-1.jpg',
+    title: 'Introduction to HTML',
+    description: 'A beginner-friendly course covering the fundamentals of HTML markup language.'
+  },
+  {
+    imageUrl: 'https://3rdwavemedia.com/demo-images/slides/maker-module-2.jpg',
+    title: 'Advanced HTML Techniques',
+    description: 'Explore advanced HTML concepts such as semantic markup and HTML5 features.'
+  },
+  {
+    imageUrl: 'https://3rdwavemedia.com/demo-images/slides/maker-module-3.jpg',
+    title: 'Mastering SCSS',
+    description: 'Learn the ins and outs of SCSS (Sassy CSS) for efficient and maintainable styling.'
+  },
+  {
+    imageUrl: 'https://3rdwavemedia.com/demo-images/slides/maker-module-4.jpg',
+    title: 'SCSS for Responsive Design',
+    description: 'Discover how to use SCSS to create responsive and adaptive web designs.'
+  },
+  {
+    imageUrl: 'https://3rdwavemedia.com/demo-images/slides/maker-module-5.jpg',
+    title: 'Building Modular CSS Architecture',
+    description: 'Explore strategies for building modular and scalable CSS architectures using SCSS.'
+  },
+  {
+    imageUrl: 'https://3rdwavemedia.com/demo-images/slides/maker-module-6.jpg',
+    title: 'Optimizing CSS Performance',
+    description: 'Learn techniques for optimizing CSS performance to enhance website loading speed.'
+  }
+];
+ 
