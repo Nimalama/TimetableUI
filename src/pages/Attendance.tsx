e1b6da1fc0f2d6d557c3cadc95c66f6c5a222091
Nimalama <arthamanlama4@gmail.com>
2024-05-27 10:36:44 +1000

    Add bar chart to the admin attendance viewing page.

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { AllAttendanceData, Attendance, getAllAttendance, getAttendance } from '../services/attendanceServices';
 import useDashboardContext from '../hooks/useChallengesDashboardContext';
 import Chart from 'chart.js/auto'; // Import Chart.js
@@ -12,6 +12,9 @@ const AttendanceComponent = () => {
     lecturersData: [],
     studentsData: []
   });
+
+  const pieChartRef = useRef<Chart | null>(null);
+  const barChartRef = useRef<Chart | null>(null);
 
   useEffect(() => {
     if (!isUserProcessing) return;
@@ -40,49 +43,102 @@ const AttendanceComponent = () => {
 
       fetchAttendance();
     }
-  }, [userInformation]);
+  }, [userInformation, isUserProcessing, isAdmin, isStudent]);
 
   useEffect(() => {
-    const renderPieChart = () => {
-      const canvas = document.getElementById('attendanceChart') as HTMLCanvasElement;
+    if (!isAdmin) {
+      const renderPieChart = () => {
+        const canvas = document.getElementById('attendanceChart') as HTMLCanvasElement;
 
-      if (canvas) {
-        new Chart(canvas, {
-          type: 'pie',
-          data: {
-            labels: ['Attended Classes', 'Remaining Classes'],
-            datasets: [
-              {
-                data: [attendance.totalAttendedClasses, attendance.totalClasses - attendance.totalAttendedClasses],
-                backgroundColor: ['#36a2eb', '#ff6384']
-              }
-            ]
-          },
-          options: {
-            responsive: true,
-            maintainAspectRatio: false
+        if (canvas) {
+          if (pieChartRef.current) {
+            pieChartRef.current.destroy();
           }
-        });
-      }
-    };
 
-    renderPieChart();
-  }, [attendance]);
+          pieChartRef.current = new Chart(canvas, {
+            type: 'pie',
+            data: {
+              labels: ['Attended Classes', 'Remaining Classes'],
+              datasets: [
+                {
+                  data: [attendance.totalAttendedClasses, attendance.totalClasses - attendance.totalAttendedClasses],
+                  backgroundColor: ['#36a2eb', '#ff6384']
+                }
+              ]
+            },
+            options: {
+              responsive: true,
+              maintainAspectRatio: false
+            }
+          });
+        }
+      };
 
-  // need to add type teacher or student to allAttendance data
+      renderPieChart();
+    }
+  }, [attendance, isAdmin]);
+
+  useEffect(() => {
+    if (isAdmin) {
+      const renderBarChart = () => {
+        const canvas = document.getElementById('attendanceBarChart') as HTMLCanvasElement;
+
+        if (canvas) {
+          if (barChartRef.current) {
+            barChartRef.current.destroy();
+          }
+
+          const lecturerNames = allAttendance.lecturersData.map((lecturer) => lecturer.name);
+          const studentNames = allAttendance.studentsData.map((student) => student.name);
+          const names = lecturerNames.concat(studentNames);
+
+          const attendancePercentages = allAttendance.lecturersData
+            .map((lecturer) => (lecturer.totalAttendedClasses / lecturer.totalClasses) * 100)
+            .concat(
+              allAttendance.studentsData.map((student) => (student.totalAttendedClasses / student.totalClasses) * 100)
+            );
+
+          barChartRef.current = new Chart(canvas, {
+            type: 'bar',
+            data: {
+              labels: names,
+              datasets: [
+                {
+                  label: 'Attendance Percentage',
+                  backgroundColor: 'rgba(75,192,192,1)',
+                  borderWidth: 1,
+                  data: attendancePercentages
+                }
+              ]
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true,
+                  max: 100
+                }
+              }
+            }
+          });
+        }
+      };
+
+      renderBarChart();
+    }
+  }, [allAttendance, isAdmin]);
+
   const mergedData = [
     ...allAttendance.lecturersData.map((ss) => {
       return { ...ss, type: 'Teacher' };
     }),
-
     ...allAttendance.studentsData.map((ss) => {
       return { ...ss, type: 'Student' };
     })
   ];
 
-  if (isAdmin)
+  if (isAdmin) {
     return (
-      <section className="fg-1">
+      <section className="fg-1 overflow-auto mb-12x">
         <div className="container">
           <div className="table-wrapper my-6x">
             <table className="common-table">
@@ -101,20 +157,22 @@ const AttendanceComponent = () => {
                     <td>{user.name}</td>
                     <td>{user.totalAttendedClasses}</td>
                     <td>{user.totalClasses}</td>
-
-                    {/* Add additional columns if needed */}
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
+          <div>
+            <canvas id="attendanceBarChart" />
+          </div>
         </div>
       </section>
     );
+  }
 
-  if (!isAdmin)
+  if (!isAdmin) {
     return (
-      <section className="fg-1">
+      <section className="fg-1 overflow-auto">
         <div className="container">
           <div className="d-flex justify-content-center my-6x">
             {attendance.totalClasses === 0 ? (
@@ -128,6 +186,7 @@ const AttendanceComponent = () => {
         </div>
       </section>
     );
+  }
 
   return <></>;
 };
